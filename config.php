b7cbdce0445c6e330d81250ec1a073bf57dc95c3
ren_kulitz <ren_kulitz@REN-KULITZ>
2019-03-31 19:27:45 -0400

    Add config File and fix the patths for NEws and GainersLosers Feature

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php 
+
+define("ROOTPATH", "../../");
+
+$csspath = ROOTPATH. "css/";
+
+$imgpath = ROOTPATH. "images/";
+
+$includepath = ROOTPATH. "includes/";
+
+$modelspath = ROOTPATH. "models/";
+
+$librarypath = ROOTPATH. "library/Chart";
+
+
+?>
+
+
+
